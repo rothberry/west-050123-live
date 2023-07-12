ba8141523850fce1b048ea8ee384abcf794ecceb
rothberry <phil.roth077@gmail.com>
2023-07-12 14:25:08 -0700

    messing with cli

--- 4-phase/06-big-o/lib/big_o.py
+++ 4-phase/06-big-o/lib/big_o.py
@@ -1,12 +1,27 @@
 from ipdb import set_trace
 from os import system
 from pprint import pp
-from helpers import term_wrap
+from helpers import term_wrap, star_line, center_string_stars
 
 # ! BIG O NOTATION
+
+# * TIME COMPLEXITY 
+class ConstantTime():
+    pass
+
+class LinearTime():
+    pass
+
+class QuadraticTime():
+    pass
 
 if __name__ == "__main__":
     system("clear")
     term_wrap("Big O Notation")
+    constant = ConstantTime()
+    linear = LinearTime()
+    quadratic = QuadraticTime()
+
 
     set_trace()
+    center_string_stars("BYE")
--- 4-phase/06-big-o/lib/helpers.py
+++ 4-phase/06-big-o/lib/helpers.py
@@ -1,9 +1,19 @@
 from os import get_terminal_size
 
+term_size = get_terminal_size().columns
+
+
 def term_wrap(string):
-    term_size = get_terminal_size().columns
+    star_line()
+    center_string_stars(string)
+    star_line()
+
+
+def star_line():
+    print("*" * term_size)
+
+
+def center_string_stars(string):
     half_size = (term_size - len(string) - 2) / 2
     half_stars = "*" * int(half_size)
-    print("*" * term_size)
     print(f"{half_stars} {string} {half_stars}")
-    print("*" * term_size)
