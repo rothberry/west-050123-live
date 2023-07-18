234f8d5ce0cdc1e1d3af11c5facec6884d806399
rothberry <phil.roth077@gmail.com>
2023-07-18 11:03:21 -0700

    data structures start

---
+++ 4-phase/09-data-structures/lib/data_structures.py
@@ -0,0 +1,12 @@
+from ipdb import set_trace
+from os import system
+from pprint import pp
+from helpers import term_wrap, star_line, center_string_stars
+
+
+if __name__ == "__main__":
+    system("clear")
+    term_wrap("DATA STRUCTURES")
+
+    set_trace()
+    center_string_stars("BYE")
